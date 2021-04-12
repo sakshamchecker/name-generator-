7f3f13f668c40ace7dae4976f133e3f119bacfe0
Saksham Checker <checkersaksham@yahoo.com>
2021-04-13 01:16:43 +0530

    Corrected name combinations

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ bn = open("boyname.txt", "r")
 gn = open("girlname.txt", "r")
 sn = open('sirname.txt', "r")
 rn = open('random.txt','w')
-for n in sn :
-    for b in bn :
-        c=b.strip() +" "+n.strip()
-        print(c)
-      
-      
+
+srn=list(sn)
+brn=list(bn)
+
+for n in srn :
+    for b in brn :
+        print(b.strip(),'',n.strip())
+  
